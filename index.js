a49c47483ed98b8eaf6f278e18ebbc243afd2e9c
Raul Gomez <raul_nouni@hotmail.com>
2020-01-16 22:16:24 -0600

    Agregando middlewares

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const logger = require('morgan') // Muestra informacion adicional de los request en la consola
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 require('./routes/views')(app)
 require('./routes/special')(app)
