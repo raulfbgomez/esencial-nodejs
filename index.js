4cdcad1696e8646de4132734fb6e82576b1a02af
Raul Gomez <raul_nouni@hotmail.com>
2020-01-16 23:13:15 -0600

    Conectando a MongoDB

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const mongo = require('./db/connect')
+
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,7 +14,23 @@ require('./routes/views')(app)
 require('./routes/api')(app)
 require('./routes/special')(app)
 
-console.log('Iniciando express.js')
-app.listen(3000, () => {
-  console.log('express ha iniciado correctamente')
-})
+async function initMongo() {
+  const db = await mongo.connect()
+  if (db) initExpress()
+}
+
+function initExpress() {
+  console.log('Iniciando express.js')
+  app.listen(3000, () => {
+    console.log('express ha iniciado correctamente')
+    process.on('SIGINT', closeApp)
+    process.on('SIGTERM', closeApp)
+  })
+}
+
+function closeApp() {
+  mongo.disconnect()
+  .then(() => process.exit(0))
+}
+
+initMongo()
